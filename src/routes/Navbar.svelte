e1942eef02ea95d28e0deedca13db4e3f52e08b7
LemuelKL <lemuellee.kl@gmail.com>
2023-01-11 13:09:41 +0000

    .

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,54 @@
+<div class="bg-black text-white flex justify-between divide-x divide-gray-400 px-1">
+	<!-- Icon & Title -->
+	<div class="flex items-center gap-1 p-2">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-7 h-7"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+			/>
+		</svg>
+		<p class="font-semibold text-2xl">LEMUEL</p>
+	</div>
+	<!--  -->
+	<div class="flex items-center gap-4">
+		<div class="ml-4 flex items-center">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				class="w-4 h-4"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+			</svg>
+			<p class="font-bold text-sm">Menu</p>
+		</div>
+		<div
+			class="h-10 w-10 bg-orange-700 flex items-center justify-center  hover:bg-orange-600 hover:cursor-pointer"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+				/>
+			</svg>
+		</div>
+	</div>
+</div>
