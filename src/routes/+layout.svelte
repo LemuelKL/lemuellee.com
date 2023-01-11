3ff1ae0d1cd2afefa7c04f617b4fb365945d6e5d
LemuelKL <lemuellee.kl@gmail.com>
2023-01-11 15:25:14 +0000

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script>
 	import Navbar from './Navbar.svelte';
-    import "./../app.css";
+	import './../app.css';
 </script>
 
 <Navbar />
-<main><slot /></main>
+<main class="bg-gray-200"><slot /></main>
+
+<style>
+	main {
+		background-image: url('texture.svg');
+		background-repeat: repeat;
+		background-position: center;
+		background-size: 2;
+	}
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,60 @@
+<div class="h-screen font-serif">
+	<div class="w-full h-1/6 bg-emerald-300 bg-signal">
+		<div class="flex justify-center items-center h-full text-3xl">
+			<div class="bg-white p-2 shadow-inner shadow-gray-900">Contact Me</div>
+		</div>
+	</div>
+	<div class="w-full h-5/6 bg-gray-200 flex flex-col items-center p-3 gap-2">
+		<div class="w-11/12 bg-gray-300 outline flex justify-center gap-4 p-3">
+			<div class="flex items-center gap-1">
+				<img src="github-mark/github-mark.png" alt="github logo" class="w-6 h-6" />
+				<a href="https://www.github.com/LemuelKL">LemueLKL</a>
+			</div>
+			<div class="flex items-center gap-1">
+				<img src="linkedin.svg" alt="github logo" class="w-6 h-6" />
+				<a href="https://www.linkedin.com/in/lemuelkl/">lemuelkl</a>
+			</div>
+			<div class="flex items-center gap-1">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+					/>
+				</svg>
+
+				<a href="mailto:lemuellee.kl@gmail.com">lemuellee.kl@gmail.com</a>
+			</div>
+		</div>
+		<div class="w-11/12 bg-gray-300 outline flex flex-col items-center gap-4 p-3">
+			<p>
+				I'm currently residing in <a
+					href="https://www.royalholloway.ac.uk/student-life/accommodation/undergraduate/kingswood-1-and-2/"
+					>Kingswood Hall</a
+				>
+				of <a href="https://www.royalholloway.ac.uk/">Royal Holloway</a> in
+				<a href="https://en.wikipedia.org/wiki/Egham">Egham, UK</a>.
+			</p>
+			<p>I speak Cantonese natively, English and Mandarin fluently.</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.bg-signal {
+		background-color: #000000;
+		background-image: url("data:image/svg+xml,%3Csvg width='84' height='48' viewBox='0 0 84 48' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h12v6H0V0zm28 8h12v6H28V8zm14-8h12v6H42V0zm14 0h12v6H56V0zm0 8h12v6H56V8zM42 8h12v6H42V8zm0 16h12v6H42v-6zm14-8h12v6H56v-6zm14 0h12v6H70v-6zm0-16h12v6H70V0zM28 32h12v6H28v-6zM14 16h12v6H14v-6zM0 24h12v6H0v-6zm0 8h12v6H0v-6zm14 0h12v6H14v-6zm14 8h12v6H28v-6zm-14 0h12v6H14v-6zm28 0h12v6H42v-6zm14-8h12v6H56v-6zm0-8h12v6H56v-6zm14 8h12v6H70v-6zm0 8h12v6H70v-6zM14 24h12v6H14v-6zm14-8h12v6H28v-6zM14 8h12v6H14V8zM0 8h12v6H0V8z' fill='%23ffffff' fill-opacity='1' fill-rule='evenodd'/%3E%3C/svg%3E");
+	}
+
+	a:hover {
+		background-color: gold;
+		text-decoration: underline;
+	}
+</style>
