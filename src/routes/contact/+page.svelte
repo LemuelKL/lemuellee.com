dd20bff1533c942a046635f588eadf28760008e0
LemuelKL <lemuellee.kl@gmail.com>
2023-01-12 07:52:12 +0000

    .

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,34 +1,22 @@
 <script>
 	import Layout from './../Layout.svelte';
+	import { GithubBrand, LinkedinBrand, EnvelopeSolid } from 'svelte-awesome-icons';
 </script>
 
 <Layout bgImgUrl="Fq14Fsk.jpeg">
+	<img slot="banner-center" src="lemuel-pro.jpg" class="h-24 rounded-full border-2" alt="lemuel" />
 	<div slot="content" class="w-full flex flex-col items-center p-4 gap-4">
 		<div class="bg-gray-200 outline w-full flex justify-center gap-4 p-3">
 			<div class="flex items-center gap-1">
-				<img src="github-mark/github-mark.png" alt="github logo" class="w-6 h-6" />
+				<GithubBrand />
 				<a href="https://www.github.com/LemuelKL">LemuelKL</a>
 			</div>
 			<div class="flex items-center gap-1">
-				<img src="linkedin.svg" alt="github logo" class="w-6 h-6" />
+				<LinkedinBrand />
 				<a href="https://www.linkedin.com/in/lemuelkl/">lemuelkl</a>
 			</div>
 			<div class="flex items-center gap-1">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-					/>
-				</svg>
-
+				<EnvelopeSolid />
 				<a href="mailto:lemuellee.kl@gmail.com">lemuellee.kl@gmail.com</a>
 			</div>
 		</div>
@@ -47,12 +35,6 @@
 </Layout>
 
 <style>
-	.bg-nature {
-		background-color: #13589976;
-		background-image: url('Fq14Fsk.jpeg');
-		background-size: cover;
-	}
-
 	a:hover {
 		background-color: gold;
 		text-decoration: underline;
