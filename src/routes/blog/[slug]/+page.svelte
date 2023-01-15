b980ae47884746a214b8d11ca23e48c9c5db7708
LemuelKL <lemuellee.kl@gmail.com>
2023-01-15 08:33:38 +0000

    Update +page.svelte

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import '/src/prism.css';  // for code highlighting
+	import '/src/prism.css'; // for code highlighting
 
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>Blog - {data.title}</title>
+</svelte:head>
+
 <div class="prose min-w-full p-3">
-	<div class="first-letter:text-3xl text-2xl font-serif ">{data.title}</div>
+	<div class="first-letter:text-3xl text-2xl font-serif">{data.title}</div>
 	<div>{data.date}</div>
 	<div class="w-full bg-gray-400 h-[1px]" />
 	<svelte:component this={data.content} />
