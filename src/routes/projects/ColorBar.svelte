4a3f8b8f6461a824d2604c192637d78c6590c4c5
LemuelKL <lemuellee.kl@gmail.com>
2023-01-16 15:48:10 +0000

    .

---
+++ src/routes/projects/ColorBar.svelte
@@ -0,0 +1,9 @@
+<div class="outline w-full h-12 flex items-center justify-between p-4">
+	<div class="w-8 h-8 bg-red-400" />
+	<div class="w-8 h-8 bg-orange-400" />
+	<div class="w-8 h-8 bg-yellow-400" />
+	<div class="w-8 h-8 bg-green-400" />
+	<div class="w-8 h-8 bg-cyan-400" />
+	<div class="w-8 h-8 bg-blue-400" />
+	<div class="w-8 h-8 bg-purple-400" />
+</div>
