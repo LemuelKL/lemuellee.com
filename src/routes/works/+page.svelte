19eb84e3528531f233e37c7638bb3b1940d0c82d
LemuelKL <lemuellee.kl@gmail.com>
2023-01-21 17:20:03 +0000

    .

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -1,6 +1,6 @@
 <div
 	class="h-full w-full bg-cover saturate-[.3]"
-	style="background-image: url('/wallpaper/pexels-carrie-johnson-1202849.jpg');"
+	style="background-image: url('/wallpaper/pexels-expect-best-351264.jpg');"
 >
 	<div class="flex h-full w-full items-center justify-center dark:bg-black/30">a</div>
 </div>
