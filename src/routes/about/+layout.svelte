c8167c955485d4a49812753fb7a99e605e6a6b5c
LemuelKL <lemuellee.kl@gmail.com>
2023-01-22 14:05:04 +0000

    .

--- src/routes/about/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<div class="absolute top-24 flex w-full justify-center">
-	<div
-		class="via-blue-gray-400 to-blue-gray-600 background-animate h-24 rounded-full bg-gradient-to-br from-gray-100 p-1"
-	>
-		<img src="/avatar/lemuel.jpg" class="h-full rounded-full" alt="lemuel" />
-	</div>
-</div>
-<div class="prose dark:prose-invert"><slot /></div>
