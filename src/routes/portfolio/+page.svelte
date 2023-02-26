bb598a64b1f25f32b23f5b98f8f62fb97ef84a76
LemuelKL <lemuellee.kl@gmail.com>
2023-02-26 15:04:49 +0000

    added portfolio welcome page

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex h-full w-full flex-col items-center justify-center">
+	<div class="flex max-w-md flex-col items-start">
+		<h1 class="text-5xl font-bold">Portfolio</h1>
+		<div class="flex gap-5">
+			<div>/pɔːtˈfəʊlɪəʊ/</div>
+			<div class="font-bold italic">noun</div>
+		</div>
+		<div class="my-2 h-[1px] w-full bg-slate-400" />
+		<div class="mt-2 flex flex-col gap-4">
+			<p>
+				1. a collection of drawings, documents, etc. that represent a person's, especially
+				an artist's, work.
+			</p>
+			<p>
+				2. a set of pieces of creative work intended to demonstrate a person's ability to a
+				potential employer.
+			</p>
+		</div>
+	</div>
+</div>
