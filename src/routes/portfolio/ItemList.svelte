51ba2d925044628d783cd358dd13687b2018ab78
LemuelKL <lemuellee.kl@gmail.com>
2023-04-07 01:53:58 +0100

    portfolio UI imrprovs

--- src/routes/portfolio/ItemList.svelte
+++ src/routes/portfolio/ItemList.svelte
@@ -30,33 +30,34 @@
 		<div>Activitiy</div>
 	</div>
 </div>
-<div
-	class="flex h-full max-h-full flex-col gap-1 divide-y divide-orange-400/50 overflow-scroll md:pr-3"
->
+<div class="flex h-full max-h-full flex-col divide-y divide-orange-500/10 overflow-scroll md:pr-3">
 	{#each items as item}
-		<a
-			class="flex border-spacing-8 flex-col p-1 hover:cursor-pointer hover:bg-orange-200/20"
-			class:bg-orange-200={item.path === activeItem}
-			class:bg-opacity-20={item.path === activeItem}
-			href={`/portfolio/${item.path}`}
-		>
-			<div class="flex items-center justify-between pt-2">
-				<div class="text-xl md:text-2xl font-bold" class:text-orange-400={item.path === activeItem}>
-					{item.meta.title}
+		<div class="flex justify-between">
+			<a
+				class="flex flex-grow border-spacing-8 flex-col px-2 pb-3 hover:cursor-pointer hover:bg-orange-200/20"
+				class:bg-orange-200={item.path === activeItem}
+				class:bg-opacity-20={item.path === activeItem}
+				href={`/portfolio/${item.path}`}
+			>
+				<div class="flex items-center justify-between pt-2">
+					<div class="text-xl font-bold md:text-2xl">
+						{item.meta.title}
+					</div>
+					<div class="bg-blue-100/50 px-1 dark:bg-blue-900/50">
+						{#if item.meta.organisation}
+							{item.meta.organisation}
+						{/if}
+					</div>
 				</div>
-				<div class="bg-blue-100/50 px-1 dark:bg-blue-900/50">
-					{#if item.meta.organisation}
-						{item.meta.organisation}
-					{/if}
+				<p class="text-gray-700 dark:text-gray-300">{item.meta.summary}</p>
+				<div class="flex items-center gap-1 text-sm">
+					{#each item.meta.tags as tag}
+						<span class="bg-zinc-100 px-1 font-light dark:bg-zinc-800">{tag}</span>
+						<div class="mx-1 h-[1px] w-1 bg-zinc-400 last:hidden" />
+					{/each}
 				</div>
-			</div>
-			<p>{item.meta.summary}</p>
-			<div class="flex items-center gap-1 text-sm">
-				{#each item.meta.tags as tag}
-					<span class="bg-zinc-100 px-1 dark:bg-zinc-800">{tag}</span>
-					<div class="mx-1 h-1 w-1 rounded-full bg-zinc-400 last:hidden" />
-				{/each}
-			</div>
-		</a>
+			</a>
+			<div class="h-full w-1" class:bg-orange-400={item.path === activeItem} />
+		</div>
 	{/each}
 </div>
