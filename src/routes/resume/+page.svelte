52dde629818de6fe87b9ec76f60891b227af5320
Lemuel Lee <lemuellee.kl@gmail.com>
2024-01-16 00:00:55 +0800

    adding resume country flags
    also remove organisation colour as not to overwhelm the visual experience.

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
 	import ResumeBlock from './ResumeBlock.svelte';
+	import * as Flag from 'svelte-flag-icons';
 
-	const identities = [
+	interface identity {
+		title: string;
+		organisation: string;
+		org_short: string;
+		org_colour: string;
+		country_code: keyof typeof Flag;
+		start_date: string;
+		end_date: string;
+	}
+
+	const identities: identity[] = [
 		{
 			title: 'Teaching Assistant',
 			organisation: 'The University of Hong Kong',
 			org_short: 'HKU',
 			org_colour: 'text-emerald-700',
+			country_code: 'Hk',
 			start_date: 'Sep 2023',
 			end_date: 'Dec 2023'
 		},
@@ -15,6 +27,7 @@
 			organisation: 'The University of Hong Kong',
 			org_short: 'HKU',
 			org_colour: 'text-emerald-700',
+			country_code: 'Hk',
 			start_date: 'Aug 2023',
 			end_date: 'Jun 2024'
 		},
@@ -23,6 +36,7 @@
 			organisation: 'Low Carbon Contracts Company',
 			org_short: 'LCCC',
 			org_colour: 'text-blue-500',
+			country_code: 'Gb',
 			start_date: 'May 2023',
 			end_date: 'Aug 2023'
 		},
@@ -31,6 +45,7 @@
 			organisation: 'Royal Holloway, University of London',
 			org_short: 'RHUL',
 			org_colour: 'text-orange-400',
+			country_code: 'Gb',
 			start_date: 'Jan 2023',
 			end_date: 'Jun 2023'
 		},
@@ -39,6 +54,7 @@
 			organisation: 'Shatin Pui Ying College',
 			org_short: 'SPYC',
 			org_colour: 'text-green-500',
+			country_code: 'Hk',
 			start_date: 'Jun 2022',
 			end_date: 'Aug 2022'
 		},
@@ -47,6 +63,7 @@
 			organisation: 'The University of Hong Kong',
 			org_short: 'HKU',
 			org_colour: 'text-emerald-700',
+			country_code: 'Hk',
 			start_date: 'Sep 2021',
 			end_date: 'Jun 2022'
 		},
@@ -55,6 +72,7 @@
 			organisation: 'Shatin Pui Ying College',
 			org_short: 'SPYC',
 			org_colour: 'text-green-500',
+			country_code: 'Hk',
 			start_date: 'Mar 2021',
 			end_date: 'Aug 2021'
 		}
@@ -74,7 +92,7 @@
 		class="relative m-auto flex w-full max-w-5xl flex-col items-center justify-center gap-2 md:flex-row lg:gap-24"
 	>
 		<div
-			class="flex w-full flex-grow flex-col justify-center gap-2 backdrop-blur-sm p-4 dark:text-zinc-200"
+			class="flex w-full flex-grow flex-col justify-center gap-2 p-4 backdrop-blur-sm dark:text-zinc-200"
 		>
 			<div class="flex flex-col md:flex-row md:items-center md:gap-5">
 				<div class="text-3xl font-bold">BEng CompSc</div>
@@ -82,6 +100,7 @@
 				<div class="text-xl font-medium">The University of Hong Kong</div>
 			</div>
 			<div class="mb-2 h-1 w-full bg-orange-400" />
+			<!-- Desktop -->
 			<table class="hidden border-separate border-spacing-y-2 border border-none md:table">
 				<tbody>
 					{#each identities as identity}
@@ -90,19 +109,30 @@
 								>{identity.start_date} - {identity.end_date}</td
 							>
 							<td class="text-lg font-semibold">{identity.title}</td>
-							<td class={identity.org_colour + ' text-right'}
-								>{identity.organisation}</td
-							>
+							<!-- <td class={identity.org_colour + ' text-right'} -->
+							<td class={'text-right'}> {identity.organisation}</td>
+							<td class="flex items-center justify-end">
+								<svelte:component
+									this={Flag[identity.country_code]}
+									icon={Flag[identity.country_code]}
+								/>
+							</td>
 						</tr>
 					{/each}
 				</tbody>
 			</table>
+			<!-- Mobile -->
 			<div class="flex w-full flex-col md:hidden">
 				{#each identities as identity}
 					<div class="flex flex-grow flex-row items-center justify-between gap-3">
 						<div class="text-lg font-semibold">{identity.title}</div>
 						<div class="flex-grow" />
-						<div class={identity.org_colour}>{identity.org_short}</div>
+						<!-- <div class={identity.org_colour}>{identity.org_short}</div> -->
+						<div>{identity.org_short}</div>
+						<svelte:component
+							this={Flag[identity.country_code]}
+							icon={Flag[identity.country_code]}
+						/>
 					</div>
 				{/each}
 			</div>
