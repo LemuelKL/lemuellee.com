f00fbef88c3236042c5cafd9f26c2e8c3945b17f
Lemuel Lee <lemuellee.kl@gmail.com>
2025-02-05 12:28:31 +0000

    update CSS matters

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,11 @@ module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts,svx}'],
     theme: {
         extend: {
-            fontFamily: {
-                manrope: 'Manrope',
-                sofia: ['Sofia Sans', 'sans-serif']
-            },
             animation: {
                 text: 'text 5s ease infinite'
             }
         }
     },
-    plugins: [require('@tailwindcss/typography')],
+    // plugins: [require('@tailwindcss/typography')],
     darkMode: 'class'
 };
