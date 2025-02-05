40ed564c85edc6b1b55d005c9b32d3d7fed17490
Lemuel Lee <lemuellee.kl@gmail.com>
2025-02-05 12:29:05 +0000

    lint

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        sveltekit(),
-    ],
+    plugins: [tailwindcss(), sveltekit()]
 });
